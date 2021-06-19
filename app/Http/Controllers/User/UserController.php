22f2d50eae76e58b9a7ef52a74bbd90f284fa00f
lercc <lercc.en@gmail.com>
2021-06-19 12:12:48 -0500

    make method to create new user(admin)

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\User;
 
 use App\Models\User;
+use App\Models\Role;
 use Illuminate\Http\Request;
-use App\Http\Requests\UserRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
 use App\Http\Resources\UserCollection;
+use App\Http\Requests\UserRequest;
 
 class UserController extends Controller
 {
@@ -16,10 +17,13 @@ class UserController extends Controller
         $users = User::paginate(15);
         return new UserCollection($users);
     }
-
+ 
     public function store(UserRequest $request)
     {
-        return $request;
+        $user = User::create($request->validated());
+        $user->password = bcrypt($user->password);
+        $user->save();
+        return new UserResource($user);
     }
 
     public function show(User $user)
--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -3,6 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use App\Rules\UserDocNumberDocType;
+use App\Models\Role;
 
 class UserRequest extends FormRequest
 {
@@ -14,16 +17,27 @@ class UserRequest extends FormRequest
 
     public function rules()
     {
+
+        $objectAdminRole =  Role::where('name','admin')->get('id');
+        $arrayIdRoles= [];
+        foreach ($objectAdminRole as $el) {
+            array_push($arrayIdRoles, $el->id);
+        }
+
         return [
-            'role_id'                    => ['required', 'numeric'],
-            'name'                       => ['required', 'max:80', 'regex:/^[\pL\s\-]+$/u'], 
-            'last_name'                  => ['required', 'max:120', 'regex:/^[\pL\s\-]+$/u'], 
-            'doc_type'                   => ['required', 'string'], 
-            'doc_number'                 => ['required', 'numeric', 'digits:11','unique:users,doc_number'], 
-            'email'                      => ['required', 'string', 'email', 'unique:users,email'], 
-            'password'                   => ['required', 'min:8'], 
-            'password_confirmation'      => ['required', 'min:8', 'same:password'],
-            'status'                     => ['required', 'string', 'max:11'], 
+            'role_id'               => ['required', 'numeric', Rule::in($arrayIdRoles)],
+            'name'                  => ['required', 'max:80', 'regex:/^[\pL\s\-]+$/u'], 
+            'last_name'             => ['required', 'max:120', 'regex:/^[\pL\s\-]+$/u'],
+
+            'doc_type'              => ['required', 'string', Rule::in(['dni', 'extranjeria'])],
+
+            'doc_number'            => ['required', 'numeric','unique:users,doc_number'],
+            'doc_number'            => $this->doc_type == 'dni' ? ['digits:8'] : ($this->doc_type == 'extranjeria' ? ['digits:11'] : new UserDocNumberDocType($this->doc_type) ), //'exclude_if:doc_type,false'
+            
+            'email'                 => ['required', 'string', 'email', 'unique:users,email'],
+            'password'              => ['required', 'min:8'], 
+            'password_confirmation' => ['required', 'min:8', 'same:password'],
+            'status'                => ['required', 'string', 'max:11', Rule::in(['activado', 'desactivado'])]
         ];
     }
 }
