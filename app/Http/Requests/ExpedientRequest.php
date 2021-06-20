6a15f779b302d595d701abd4acafccdd9e7180ee
lercc <lercc.en@gmail.com>
2021-06-20 15:38:50 -0500

    avance expediente

--- app/Http/Requests/ExpedientRequest.php
+++ app/Http/Requests/ExpedientRequest.php
@@ -3,6 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Processor;
+use App\Models\Employee;
+use Illuminate\Validation\Rule;
 
 class ExpedientRequest extends FormRequest
 {
@@ -15,18 +18,28 @@ class ExpedientRequest extends FormRequest
 
     public function rules()
     {
-
+        $objectProcessors =  Processor::get('id');
+        $arrayIdProcessor= [];
+        foreach ($objectProcessors as $el) {
+            array_push($arrayIdProcessor, $el->id);
+        }
+        
+        $objectEmployees =  Employee::get('id');
+        $arrayIdEmployee= [];
+        foreach ($objectEmployees as $el) {
+            array_push($arrayIdEmployee, $el->id);
+        }
 
         return [
             // 'user_id'         => ['required', 'numeric'],
-            // 'processor_id'    => ['nullable', 'numeric', Rule::in($arrayIdProcessor)],
-            // 'employee_id'     => ['nullable', 'numeric', Rule::in($arrayIdEmployee)],
+            'processor_id'    => ['nullable', 'numeric', Rule::in($arrayIdProcessor)],
+            'employee_id'     => ['nullable', 'numeric', Rule::in($arrayIdEmployee)],
             'code'            => ['required', 'alpha_num','max:30'],
             'document_type'   => ['required', 'string', 'max:200'],
             'header'          => ['required', 'string', 'max:220'],
             'subject'         => ['required', 'string', 'max:200'],
-            'folios'          => ['required', 'numeric', 'digits:3'],
-            'file'            => ['required', 'string'],
+            'folios'          => ['required', 'numeric', 'min:1', 'max:100'],
+            'file'            => ['required', 'mimes:pdf,docx,doc,xlsx,xls,zip,rar,pptx,ppt,png,jpg,jpeg'],
             'status'          => ['required', 'string', 'max:11', Rule::in(['activado', 'desactivado'])],
         ];
     }
