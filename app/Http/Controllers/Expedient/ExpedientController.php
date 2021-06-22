50754e5269c42ff85f08dfed2e795f990f9d134a
lercc <lercc.en@gmail.com>
2021-06-22 09:59:44 -0500

    make update method on ExpedientController

--- app/Http/Controllers/Expedient/ExpedientController.php
+++ app/Http/Controllers/Expedient/ExpedientController.php
@@ -6,6 +6,7 @@ use App\Models\Expedient;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ExpedientRequest;
+use App\Http\Requests\ExpedientUpdateRequest;
 use App\Http\Resources\ExpedientResource;
 use App\Http\Resources\ExpedientCollection;
 
@@ -30,34 +31,11 @@ class ExpedientController extends Controller
         return new ExpedientResource($expedient);
     }
 
-    public function update(Request $request, Expedient $expedient)
+    public function update(ExpedientUpdateRequest $request, Expedient $expedient)
     {
-        // 
-        // 'processor_id',
-        // 'employee_id',
-        // 'code',
-        // 'document_type',
-        // 'header',
-        // 'subject', 
-        // 'folios',
-        // 'file',
-        // 'status'
-
-
-        // $expedient->update($request->except('file'));
-        
-        // if ($request->hasFile('file')) {
-        //     // BORAR IMG ANTIGUA DEL STORAGE
-        //     Storage::disk('public')->delete($expedient->file);
-
-        //     //GUARDAR IMG NUEVA EN EL STORAGE
-        //     $file = $request->file('file')->store('file/expedients', 'public');
-
-        //     //ACTUALZIAR BD
-        //     $expedient->file = $file;
-        //     $voucher->update();
-        // }
-        // return new ExpedientResource($expedient);
+        // campos a actualizar 'status'
+        $expedient->update($request->validated());
+        return new ExpedientResource($expedient);
     }
 
     public function destroy(Expedient $expedient)
