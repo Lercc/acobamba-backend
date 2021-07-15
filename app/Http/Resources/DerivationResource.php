4c79d598d6e2c303b3bd66beaeece78e440910da
pbp15 <gatopbp123@gmail.com>
2021-07-14 21:00:18 -0500

    seguimos

--- app/Http/Resources/DerivationResource.php
+++ app/Http/Resources/DerivationResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Employee;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class DerivationResource extends JsonResource
@@ -15,6 +16,9 @@ class DerivationResource extends JsonResource
     public function toArray($request)
     {
        // return parent::toArray($request);
+       $employee = Employee::where('user_id', $this->user_id)->get();
+       $employee = $employee[0];
+
         return [
             'type' =>  'Derivation',
             'id'   =>  (string) $this->id,
@@ -24,13 +28,13 @@ class DerivationResource extends JsonResource
                 'expedient_code' => $this->expedient->code, 
                 'user_id'        => $this->user_id,
                 'user_name'      => "{$this->user->last_name} {$this->user->name}",
+                'user_area'      => $employee->office_id ? $employee->office->name : $employee->suboffice->name,
                 'employee_id'    => $this->employee_id,
                 'employee_name'  => $this->employee->user->name,
                 'employee_area'  => $this->employee->office_id ? $this->employee->office->name : $this->employee->suboffice->name,
                 'status'         => $this->status,
                 'createdAt'        => date('m/d/Y H:i:s a', strtotime($this->created_at))
             ],
-
             'relationships' => [
                 'expedient' => [
                     'links' => [
